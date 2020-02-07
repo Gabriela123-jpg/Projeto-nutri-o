78460f64bd5dfa48694911d5eec08f45ecad4d30
gabriela <gabriela.rocha71@etecia.com.br>
2020-02-06 22:03:47 -0300

    Conta do primeiro IMC

--- js/java.js
+++ js/java.js
@@ -1,3 +1,14 @@
 	//console.log("falha ao carregar!");            
 	let titulo = document.querySelector("#titulo");
-	titulo.textContent = "Etecia Nutrição e Dietética";
+   titulo.textContent = "Etecia Nutrição e Dietética";
+
+   //pegar o peso
+ let paciente = document.querySelector(".paciente");
+ let tdPeso = paciente.querySelector(".info-peso");
+ let peso = tdPeso.textContent;
+ //pegar altura
+ let tdAltura = paciente.querySelector(".info-altura");
+ let Altura = tdAltura.textContent;
+  // calcular imc
+  let imc = peso /(Altura * Altura);
+  console.log(imc)
