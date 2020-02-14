2cfeec1a214c328988a937c286288a9221c8e534
Gabriela123-jpg <gabriela.rocha71@etecia.com.br>
2020-02-13 21:25:40 -0300

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,14 +1,16 @@
-for (var i = 0 ; i<5; i++){
-  console.log(i)
-}
+   //pegar o peso
+   let pacientes = document.querySelectorAll(".paciente");
+
+   for(var i = 0 ; i< pacientes.length; i++){
+
+
+    let paciente  = pacientes[i];
 
 
 	//console.log("falha ao carregar!");            
 	let titulo = document.querySelector("#titulo");
-   titulo.textContent = "Etecia Nutrição e Dietética";
+ titulo.textContent = "Etecia Nutrição e Dietética";
 
-   //pegar o peso
- let paciente = document.querySelector(".paciente");
  let tdPeso = paciente.querySelector(".info-peso");
  let peso = tdPeso.textContent;
  let pesovalido=true
@@ -17,6 +19,28 @@ for (var i = 0 ; i<5; i++){
  let Altura = tdAltura.textContent;
  let alturavalido= true
  // calcular imc
-  let imc = peso /Altura **2;
+ let imc = peso /Altura **2;
 
-  
+ //validar dados
+ if(peso<=0 || peso > 1000){
+   console.error("peso invalido!");
+   pesovalido= false 
+   tdimc.textContent="Peso invalido" 
+ }
+
+ if (Altura<=0){
+  console.error("Altura invalida!");
+  alturavalido= false
+}
+
+        // escrever o IMC
+        if(pesovalido && alturavalido){
+        //calcular o imc
+        let imc = peso/Altura ** 2;
+        let tdimc = paciente.querySelector(".info-imc");
+        tdimc.textContent = imc.toFixed(2);
+      }
+    }
+
+
+
