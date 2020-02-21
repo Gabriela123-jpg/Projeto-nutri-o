7e698f76390acbe5fe338717bdffcd190648be57
Gabriela123-jpg <gabriela.rocha71@etecia.com.br>
2020-02-20 22:04:05 -0300

    add td, tr

--- js/java.js
+++ js/java.js
@@ -2,11 +2,11 @@
    let pacientes = document.querySelectorAll(".paciente");
 //maif facil que usar o FOR ou seja ao inves de usar o for,para cada paciente se coloca FOREACH e diminui o codigo
 //função anonima
- pacientes.forEach ( function (paciente){
-let peso = paciente.querySelector(".info-peso").textContent;
+pacientes.forEach ( function (paciente){
+  let peso = paciente.querySelector(".info-peso").textContent;
 
-let titulo = document.querySelector("#titulo");
-titulo.textContent = "Etecia Nutrição e Dietética";
+  let titulo = document.querySelector("#titulo");
+  titulo.textContent = "Etecia Nutrição e Dietética";
  //pegar altura
  let Altura = paciente.querySelector(".info-altura").textContent;
  
@@ -33,8 +33,11 @@ if (Altura<=0){
         tdimc.textContent = imc.toFixed(2);
       }
     });
+/*
 let titulo = document.querySelector("#titulo");
-titulo.onclick = mostrarMensagem;
+titulo.addEventListener ("click" , mostrarMensagem);
+titulo.addEventListener ("click" , trocarCor);
+
 function mostrarMensagem(){
   console.log("evento aconteceu");
 }
@@ -42,3 +45,58 @@ function trocarCor(){
   let cor = parseInt(Math.random()*255);
   titulo.style.color = "rgb("+cor+","+cor+","+cor+")";
 }
+*/
+let botao = document.querySelector("#botao-cadastrar");
+
+botao.addEventListener("click" ,function (evento){
+  evento.preventDefault();
+    //pegar valores da form
+    let formulario = document.querySelector("#form-paciente");
+    let nome = formulario.nome.value;
+    let peso = formulario.peso.value;
+    let altura  = formulario.altura.value;
+    let gordura = formulario.gordura.value;
+    
+    //criar linha
+    let table = document.querySelector("#tabela-pacientes");
+    let tr = document.createElement("tr");
+    tr.classList.add("paciente");
+
+    //Criar td nome
+    let tdNome = document.createElement("td");
+    tdNome.textContent = nome;
+    table.appendChild(tdNome);
+
+    table.appendChild(tr);
+
+    //Criar td peso
+
+    let tdPeso = document.createElement("td");
+    tdPeso.textContent = peso;
+    table.appendChild(tdPeso);
+
+    table.appendChild(tr);
+
+    //Criar td altura 
+
+    let tdAltura = document.createElement("td");
+    tdAltura.textContent = altura;
+    table.appendChild(tdAltura);
+
+    table.appendChild(tr);
+
+    // Criar td gordura
+    let tdGordura = document.createElement("td");
+    tdGordura.textContent = gordura;
+    table.appendChild(tdGordura);
+
+    table.appendChild(tr);
+
+
+    //console.log(tr);
+
+  });
+
+
+
+
